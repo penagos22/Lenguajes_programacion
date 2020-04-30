83758ecf04a2099615cb714a4f2660817ce05a5b
penagos22 <64174795+penagos22@users.noreply.github.com>
2020-04-29 19:59:42 -0500

    Create Persona.java

---
+++ Primer Corte/EjemploPatronMVC/Modelo/Persona.java
@@ -0,0 +1,70 @@
+package Modelo;
+
+public class Persona {
+    // encapsulamiento + tipo de varibale + nombre de la variable = declara variable
+    private String name;
+    private String apellido;
+    private int edad;
+    private String email;
+    private int estatura;
+    private int cedula;
+    
+    //Metodos
+    public void cantar (){
+        
+    }
+    
+    public void programar (){
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(int estatura) {
+        this.estatura = estatura;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+    
+    
+}
