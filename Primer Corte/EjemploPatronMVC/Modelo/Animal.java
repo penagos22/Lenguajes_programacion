bcaa0ecf55432074c29391ebc3f7a2fb819f9dca
penagos22 <64174795+penagos22@users.noreply.github.com>
2020-04-29 19:59:08 -0500

    Create Animal.java

---
+++ Primer Corte/EjemploPatronMVC/Modelo/Animal.java
@@ -0,0 +1,37 @@
+package Modelo;
+
+public class Animal {
+    
+    private String nombre;
+    private String raza;
+    private int edad;
+    
+    public void morder (){
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }    
+    
+}
